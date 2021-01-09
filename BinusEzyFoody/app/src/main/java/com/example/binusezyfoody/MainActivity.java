31845409912eaa369e8d7fc1646da481d6b1227e
Vincent Rohandy <laurensius.rohandy@binus.ac.id>
2021-01-10 00:39:52 +0700

    initialize

---
+++ BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.binusezyfoody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickDrinksButton(View view) {
+        Intent intent = new Intent(this, DrinksActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickSnacksButton(View view) {
+        Intent intent = new Intent(this, SnacksActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickFoodsButton(View view) {
+        Intent intent = new Intent(this, FoodActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickTopUpButton(View view) {
+        Intent intent = new Intent(this, TopUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickMyOrder(View view) {
+        Intent intent = new Intent(this, MyOrderActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/TopUpActivity.java
@@ -0,0 +1,20 @@
+package com.example.binusezyfoody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class TopUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_up);
+    }
+    public void onClickBackButton(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
