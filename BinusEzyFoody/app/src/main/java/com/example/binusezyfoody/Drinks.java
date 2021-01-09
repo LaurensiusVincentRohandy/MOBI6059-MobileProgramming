2ed1349f7565556d0e6e91e283992ff60af91c7c
Vincent Rohandy <laurensius.rohandy@binus.ac.id>
2021-01-10 00:35:05 +0700

    nambah

--- BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/Drinks.java
+++ BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/Drinks.java
@@ -1,23 +1,37 @@
 package com.example.binusezyfoody;
 
 public class Drinks {
-    String name;
-    int price, qty;
+    private String name;
+    private int price, qty, stock, imageResourceID;
+
+    public static final Drinks[] drinks = {
+            new Drinks("Air Mineral", 4000, 24, R.drawable.air),
+            new Drinks("Jus Apel", 13000, 25, R.drawable.apel),
+            new Drinks("Jus Mangga", 6000, 10, R.drawable.gbr_mangga),
+            new Drinks("Jus Alpukat", 20000, 5, R.drawable.alpukat),
+    };
+
+    private Drinks(String name, int price, int stock, int imageResourceID) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.imageResourceID = imageResourceID;
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public int getStock() {
+        return stock;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
     }
 
     public int getQty() {
--- BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/DrinksActivity.java
+++ BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/DrinksActivity.java
@@ -11,44 +11,50 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class DrinksActivity extends AppCompatActivity {
-    ArrayList<Drinks> drinksList = new ArrayList<>();
-    Drinks drinks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
+
+        String[] drinkNames = new String[Drinks.drinks.length];
+        for (int i = 0; i < drinkNames.length; i++){
+            drinkNames[i] = Drinks.drinks[i].getName();
+        }
+
+        int[] drinkPrices = new int[Drinks.drinks.length];
+        for (int i = 0; i < drinkPrices.length; i++){
+            drinkPrices[i] = Drinks.drinks[i].getPrice();
+        }
+
+        int[] drinkStocks = new int[Drinks.drinks.length];
+        for (int i = 0; i < drinkStocks.length; i++){
+            drinkStocks[i] = Drinks.drinks[i].getStock();
+        }
+
+        int[] drinkImages = new int[Drinks.drinks.length];
+        for (int i = 0; i < drinkPrices.length; i++){
+            drinkPrices[i] = Drinks.drinks[i].getPrice();
+        }
     }
 
     public void onClickBuyWater(View view) {
         Intent intent = new Intent(this, OrderActivity.class);
-        drinks.setName("Air Mineral");
-        drinks.setPrice(37500);
-        drinksList.add(drinks);
         startActivity(intent);
     }
 
     public void onClickBuyApel(View view) {
         Intent intent = new Intent(this, OrderActivity.class);
-        drinks.setName("Jus Apel");
-        drinks.setPrice(50000);
-        drinksList.add(drinks);
         startActivity(intent);
     }
 
     public void onClickBuyMango(View view) {
         Intent intent = new Intent(this, OrderActivity.class);
-        drinks.setName("Jus Mangga");
-        drinks.setPrice(15000);
-        drinksList.add(drinks);
         startActivity(intent);
     }
 
     public void onClickBuyAlpukat(View view) {
         Intent intent = new Intent(this, OrderActivity.class);
-        drinks.setName("Jus Alpukat");
-        drinks.setPrice(10000);
-        drinksList.add(drinks);
         startActivity(intent);
     }
 
