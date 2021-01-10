d1fabd639fbe4f316d3cb23a85154ca4ceaa8192
Vincent Rohandy <laurensius.rohandy@binus.ac.id>
2021-01-10 19:00:07 +0700

    Add TopUp

---
+++ BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/TopUp.java
@@ -0,0 +1,13 @@
+package com.example.binusezyfoody;
+
+public class TopUp {
+    String balance = "20000";
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+}
--- BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/TopUpActivity.java
+++ BinusEzyFoody/app/src/main/java/com/example/binusezyfoody/TopUpActivity.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class TopUpActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+    TopUp topUp = new TopUp();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_up);
+        String balance = topUp.getBalance();
+        TextView display = (TextView) findViewById(R.id.currBalance);
+        display.setText(balance);
     }
+
     public void onClickBackButton(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
